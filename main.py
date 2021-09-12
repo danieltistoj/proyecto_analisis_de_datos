c6c98fa3449dbe52c1acf5fb27754945ebcdccf1
danieltistoj <josetisrey@gmail.com>
2021-09-11 18:11:51 -0600

    registro de palabras

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import pynput.keyboard
+#trabajar con palabras
+lista_tecla=[] #En esta lista se guardaran las palabras
+def imprimir():
+    tecla = ''.join(lista_tecla)
+    print(tecla)
 
 def presiona(key):
     key1 = convertir(key)
-    print("Tecla presionada: {}".format(key1))
-def libera(key):
-    key1 = convertir(key)
-    print("Tecla liberada: {}".format(key1))
-    if str(key) == "Key.esc":
-        print("entro")
-        print("saliendo...")
+    #Key es con la letra k en mayuscula
+    if key1 == "Key.esc":
+        print("Saliendo...")
+        imprimir()
         return False
+    #Si es igual al espacio
+    elif key1 == "Key.space":
+        lista_tecla.append(" ")
+    #Si es igual a una letra
+    else:
+        lista_tecla.append(key1)
+
 def convertir(key):
     if isinstance(key,pynput.keyboard.KeyCode):
         return key.char
     else:
         return str(key)
 
-with pynput.keyboard.Listener(on_press=presiona,on_release=libera) as liste:
+with pynput.keyboard.Listener(on_press=presiona) as liste:
     liste.join()
