fab502facf793ae00e4bd4c17ae80f6e3edbc589
danieltistoj <josetisrey@gmail.com>
2021-09-15 00:48:27 -0600

    convertir lista de procesos en un diccionario

--- subproceso.py
+++ subproceso.py
@@ -1,10 +1,12 @@
 import wmi
 def ultimos_procesos():
+    diccionario ={}
     f = wmi.WMI()
     procesos = ""
     #print("pid   Process name")
 
     for process in f.Win32_Process():
-       procesos = procesos  +  f"{process.ProcessId:<10} {process.Name}"+"\n"
-    return procesos
+        diccionario[f"{process.ProcessId}"] = f" {process.Name}"
+    return diccionario
+
 #https://es.acervolima.com/2021/02/09/python-obtener-una-lista-de-los-procesos-en-ejecucion/
