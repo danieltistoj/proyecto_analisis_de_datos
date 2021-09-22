f449ec9339f3576da899d3721b3b9d5d58dc42ca
danieltistoj <josetisrey@gmail.com>
2021-09-21 18:19:52 -0600

    insertar equipo y ejecucion mysql

--- consultas_mysql.py
+++ consultas_mysql.py
@@ -9,25 +9,29 @@ def existe_equipo(ip):
     try:
         id = -1
         db.cursor.execute(query)
-        datos = db.cursor.fetchall()
-        for dato in datos:
-            id = dato
+        while True:
+            fila = db.cursor.fetchone()
+            if fila == None:
+                break
+            id = fila[0]
+
 
         if id>0:
+            print("se encontro la id: {}".format(id))
             return id
         else:
             insertar_equipo(ip) #Inserta el equipo en la base de datos
             return obtener_ultimo_id("equipo")
     except:
-        print("Errro consulta existe equipo")
+        print("Error consulta existe equipo")
 
 def obtener_ultimo_id(tabla):
     query = "SELECT MAX(id) FROM {}".format(tabla)
     try:
         db.cursor.execute(query)
-        dato = db.cursor.fetchall()
-        print(dato)
-        return dato
+        dato = db.cursor.fetchone()
+        print(dato[0])
+        return dato[0]
     except:
         print("Error al buscar ultimo id")
 
@@ -40,6 +44,22 @@ def insertar_equipo(ip):
         print("Error al insertar equipo")
 
 def insertar_ejecucion(tiempo_inicial,tiempo_final,ip):
-    pass
+    id_equipo = existe_equipo(ip)
+    query = "INSERT INTO ejecucion(fecha_hora_inicio,fecha_hora_final,id_equipo) VALUES('{}','{}','{}')".format(tiempo_inicial,tiempo_final,id_equipo)
+    try:
+        db.cursor.execute(query)
+        print("Insertar ejecucion correctamente")
+    except:
+        print("Error al insertar ejecucion")
+
+def mostrar_equipos():
+    query = "SELECT * FROM equipo"
+    db.cursor.execute(query)
+    while(True):
+        row = db.cursor.fetchone()
+        if row == None:
+            break
+        print(row[0])
 
 
+insertar_ejecucion("","","192.168.191.82")
